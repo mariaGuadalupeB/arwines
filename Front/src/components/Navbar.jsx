9fdbe251e37a579cf9707c96c5e5ed6d06e61ece
ttronge <tomastronge@gmail.com>
2021-03-17 10:16:16 -0300

    error msg

--- Front/src/components/Navbar.jsx
+++ Front/src/components/Navbar.jsx
@@ -4,7 +4,7 @@ import { Link, useHistory } from 'react-router-dom'
 import { useDispatch, useSelector } from 'react-redux';
 
 import { userLogout } from "../store/user"
-import {resetCart_items} from "../store/cart"
+import { resetCart_items } from "../store/cart"
 
 import Button from '@material-ui/core/Button';
 import styles from '../styles/navbar.module.css'
@@ -12,37 +12,37 @@ import styles from '../styles/navbar.module.css'
 
 const Navbar = () => {
     const history = useHistory()
-    const dispatch = useDispatch(); 
+    const dispatch = useDispatch();
 
     const user = useSelector((state) => state.user)
     const cart_items = useSelector((state) => state.cart_items)
     const [query, setQuery] = React.useState('');
-    
-    const isLoggedIn = Object.keys(user).length 
-    
-    const handleQuery = string => { 
+
+    const isLoggedIn = Object.keys(user).length
+
+    const handleQuery = string => {
         const querySearch = string
         setQuery('');
-        string.trim().length && history.push(`/search/${querySearch}`) 
-    };    
+        string.trim().length && history.push(`/search/${querySearch}`)
+    };
 
     const logOutHandler = () => {
-        if(isLoggedIn) {
+        if (isLoggedIn) {
             const token = user.token
             axios.put("http://localhost:5000/api/cart", cart_items, {
                 headers: { Authorization: `Bearer ${token}` }
             })
-            .then(({data}) => data)
-            
+                .then(({ data }) => data)
+
             localStorage.clear()
-            
+
             dispatch(userLogout())
             dispatch(resetCart_items())
 
             history.push("/")
-        }else{
+        } else {
             history.push("/login")
-        }  
+        }
     }
 
     return (
@@ -53,20 +53,20 @@ const Navbar = () => {
                     <Link to="/">
                         <img src="https://www.ubp.edu.ar/wp-content/uploads/2014/09/logo-de-geovinar-pin-01.png" alt="arwines" />
                     </Link>
-                    
-                    <div style={{display: 'flex'}}>
 
-                        <input 
-                            type="text" 
-                            placeholder="Busca productos marcas y mas..." 
-                            value={query} 
+                    <div style={{ display: 'flex' }}>
+
+                        <input
+                            type="text"
+                            placeholder="Busca productos marcas y mas..."
+                            value={query}
                             onChange={e => setQuery(e.target.value)}
                         />
-                        
-                        <Button 
-                            variant="outlined" 
-                            color='primary' 
-                            style={{height: '2.7em', margin: '0.9em 0 0 0.4em', backgroundColor: '#38182F', color: 'white'}} 
+
+                        <Button
+                            variant="outlined"
+                            color='primary'
+                            style={{ height: '2.7em', margin: '0.9em 0 0 0.4em', backgroundColor: '#38182F', color: 'white' }}
                             onClick={() => handleQuery(query)}
                         >
                             Search
@@ -79,31 +79,31 @@ const Navbar = () => {
                 <div className={styles.barraI}>
                     <div className={styles.botonLogIn}>
                         <div onClick={logOutHandler}>
-                            <Button variant="outlined" color='primary'  style={{backgroundColor: '#38182F', color: 'white', textDecoration: 'underline'}}>  { isLoggedIn ? "Log Out" : "Sign In" } </Button>
+                            <Button variant="outlined" color='primary' style={{ backgroundColor: '#38182F', color: 'white', textDecoration: 'underline' }}>  {isLoggedIn ? "Log Out" : "Sign In"} </Button>
                         </div>
                     </div>
-                    
+
                     <div className={styles.botonLogOut}>
                         {
-                            !isLoggedIn ? 
-                            (
-                                <Link to="/register">
-                                    <Button variant="contained" style={{backgroundColor: '#38182F', color: 'white', textDecoration: 'underline'}}> Register</Button>
-                                </Link>
-                            ) 
-                            : 
-                            (
-                            <div className={styles.carrito}>
-                            <Link to='/cart'><span className="material-icons">shopping_cart</span></Link>
-                            </div>
-                            )
-                                    
+                            !isLoggedIn ?
+                                (
+                                    <Link to="/register">
+                                        <Button variant="contained" style={{ backgroundColor: '#38182F', color: 'white', textDecoration: 'underline' }}> Register</Button>
+                                    </Link>
+                                )
+                                :
+                                (
+                                    <div className={styles.carrito}>
+                                        <Link to='/cart'><span className="material-icons" >shopping_cart</span></Link>
+                                    </div>
+                                )
+
                         }
                     </div>
-                    
+
                 </div>
             </div>
-        </div>
+        </div >
 
     )
 }

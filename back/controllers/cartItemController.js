4da68fb0c415b03df12de84680cba5da95480314
ropino <rocioailinpino@gmail.com>
2021-03-17 16:18:37 -0300

    vista del carrito

---
+++ back/controllers/cartItemController.js
@@ -0,0 +1,11 @@
+const { Cart, User, Product, Cart_item } = require("../db/models");
+
+
+const cartItemController = {};
+
+cartItemController.deleteItem = (req, res, next) => {
+
+}
+
+
+module.exports = cartItemController;

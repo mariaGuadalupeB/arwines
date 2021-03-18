d0677cb1a2cf24757dc1075ce62cf471c45b1ac7
Frean <fespeche@gmail.com>
2021-03-18 00:19:26 -0300

    probando

--- Front/src/components/SingleWine.jsx
+++ Front/src/components/SingleWine.jsx
@@ -1,39 +1,38 @@
 
 import React, { useState } from "react";
-import style from "../styles/SingleProducts.module.css"
-import { useDispatch, useSelector } from 'react-redux';
-import wineReducer, { setWine } from '../store/wine';
-import { Link } from 'react-router-dom'
-import { saveCartItems } from '../store/cart'
-import { Button, Box, Grid } from "@material-ui/core"
+import style from "../styles/SingleProducts.module.css";
+import { useDispatch, useSelector } from "react-redux";
+import wineReducer, { setWine } from "../store/wine";
+import { Link } from "react-router-dom";
+import { saveCartItems } from "../store/cart";
+import { Button, Box, Grid } from "@material-ui/core";
 import { useHistory } from "react-router-dom";
-import MenuItem from '@material-ui/core/MenuItem';
-import FormControl from '@material-ui/core/FormControl';
-import Select from '@material-ui/core/Select';
-import { useStyles } from '../themes/themesConfig'
+import MenuItem from "@material-ui/core/MenuItem";
+import FormControl from "@material-ui/core/FormControl";
+import Select from "@material-ui/core/Select";
+import { useStyles } from "../themes/themesConfig";
 
 export default function SingleWine({ match }) {
   const dispatch = useDispatch();
-  const selectedWine = useSelector(state => state.selectedWine)
-  const user = useSelector((state) => state.user)
-  const isLoggedIn = Object.keys(user).length
-  const [quantity, setQuantity] = useState(0)
+  const selectedWine = useSelector((state) => state.selectedWine);
+  const user = useSelector((state) => state.user);
+  const isLoggedIn = Object.keys(user).length;
+  const [quantity, setQuantity] = useState(0);
   const history = useHistory();
-  const itmz = JSON.parse(localStorage.getItem("cart_items"))
-  const [items, setItems] = useState(itmz)
-  const [stock, setStock] = useState([])
+  const itmz = JSON.parse(localStorage.getItem("cart_items"));
+  const [items, setItems] = useState(itmz);
+  const [stock, setStock] = useState([]);
 
   React.useEffect(() => {
-    dispatch(setWine(parseInt(match.params.id)))
-      .then((wine) => {
-        const value = wine.payload.quantity > 6 ? 6 : wine.payload.quantity
+    dispatch(setWine(parseInt(match.params.id))).then((wine) => {
+      const value = wine.payload.quantity > 6 ? 6 : wine.payload.quantity;
 
-        setStock(Array.from({ length: value }, (_, i) => i + 1))
-      });
-  }, [])
-
+      setStock(Array.from({ length: value }, (_, i) => i + 1));
+    });
+  }, []);
 
   const AddProduct = () => {
+
     let alreadyExisted = false
     const updatedCart = items.map(cart_item => {
       if (cart_item.productId === selectedWine.id) {
@@ -69,6 +68,7 @@ export default function SingleWine({ match }) {
             <div className={style.boxPrice}>
               <h3 className={style.precio}>Precio: $ {selectedWine.price}</h3>
             </div>
+
             <p className={style.description}>Descripcion: {selectedWine.description}</p>
 
             {/* {
@@ -110,22 +110,42 @@ export default function SingleWine({ match }) {
                 null
             } */}
 
-
-
-
-
           </div>
         </Grid>
       </Grid>
       <hr />
 
       <div>
+        {isLoggedIn ? (
+          <>
+            <select
+              name="quantity"
+              onChange={(e) => setQuantity(e.target.value)}
+            >
+              <option value="">0</option>
+              <option value="1">1</option>
+              <option value="2">2</option>
+              <option value="3">3</option>
+            </select>
+            {quantity ? (
+              <Button variant="contained" color="primary" onClick={AddProduct}>
+                AGREGAR
+              </Button>
+            ) : (
+              <Button
+                variant="contained"
+                color="primary"
+                onClick={AddProduct}
+                disabled
+              >
+                AGREGAR
+              </Button>
+            )}
+          </>
+        ) : null}
 
-
-
+        <hr />
       </div>
-    </div >
-
-
+    </div>
   );
 }

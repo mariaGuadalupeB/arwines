21a69efa14df885b011a0635df3b934682853a56
mariaGuadalupeB <mariagballesi@gmail.com>
2021-03-18 08:37:45 -0300

    cambios en contenido seed footer search

--- Front/src/store/wines.js
+++ Front/src/store/wines.js
@@ -8,10 +8,14 @@ export const setWines = createAsyncThunk('SET_WINES', () => {
 })
 
 export const queryWines = createAsyncThunk('QUERY_WINES', string => {
-  const categoryPromise = axios.get(`${baseUrl}/category/${string}/products`).then(res => res.data);
-  const productPromise = axios.get(`${baseUrl}/product?name=${string}`).then(res => res.data);
+  const categoryPromise = axios.get(`${baseUrl}/category/${string}/products`).then(res => res.data).catch(err => err)
+  //catcherar esto para no caiga elp primise ;
+  const productPromise = axios.get(`${baseUrl}/product?name=${string}`).then(res => res.data).catch(err => err);
   
   return Promise.any([categoryPromise, productPromise])
+  //.catch( para recibier el errror )
+  // finally
+
   
 
   
